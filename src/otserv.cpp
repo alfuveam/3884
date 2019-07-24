8e71d6fee00983349370269c13b3a93ca37c8e2b
Aspiraboo <felipe.b.t@hotmail.com>
2019-07-24 04:34:59 -0300

    Chagen RSA function file

--- src/otserv.cpp
+++ src/otserv.cpp
@@ -73,6 +73,56 @@
 	}
 #endif
 
+#if OPENSSL_VERSION_NUMBER < 0x10100000L
+
+// Provide compatibility across OpenSSL 1.02 and 1.1.
+static int RSA_set0_key(RSA *r, BIGNUM *n, BIGNUM *e, BIGNUM *d) {
+  /* If the fields n and e in r are NULL, the corresponding input
+   * parameters MUST be non-NULL for n and e.  d may be
+   * left NULL (in case only the public key is used).
+   */
+  if ((r->n == NULL && n == NULL) || (r->e == NULL && e == NULL)) {
+    return 0;
+  }
+
+  if (n != NULL) {
+    BN_free(r->n);
+    r->n = n;
+  }
+  if (e != NULL) {
+    BN_free(r->e);
+    r->e = e;
+  }
+  if (d != NULL) {
+    BN_free(r->d);
+    r->d = d;
+  }
+
+  return 1;
+}
+
+ static int RSA_set0_factors(RSA *r, BIGNUM *p, BIGNUM *q) {
+    /* If the fields p and q in r are NULL, the corresponding input
+     * parameters MUST be non-NULL.
+     */
+    if ((r->p == NULL && p == NULL)
+        || (r->q == NULL && q == NULL))
+        return 0;
+
+    if (p != NULL) {
+        BN_free(r->p);
+        r->p = p;
+    }
+    if (q != NULL) {
+        BN_free(r->q);
+        r->q = q;
+    }
+
+    return 1;
+ }
+
+#endif  // OPENSSL_VERSION_NUMBER < 0x10100000L
+
 RSA* g_RSA;
 ConfigManager g_config;
 Game g_game;
@@ -534,8 +584,8 @@ void otserv(StringVec, ServiceManager* services)
 	BN_dec2bn(&bn, g_config.getString(ConfigManager::RSA_MODULUS).c_str());
 	BN_dec2bn(&be, g_config.getString(ConfigManager::RSA_PUBLIC).c_str());
 
-    set_rsa_key(g_RSA, bn, be, bd);
-    set_rsa_factors(g_RSA, bp, bq);
+    RSA_set0_key(g_RSA, bn, be, bd);
+    RSA_set0_factors(g_RSA, bp, bq);
 	
 	if(!RSA_check_key(g_RSA))
 	{
